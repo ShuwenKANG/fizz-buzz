2cfee1565527758efff0b3a0a0a12dd79e63226e
Shawn <shuwen.kang@exxonmobil.com>
2020-01-09 21:30:33 +0800

    add test: should return 2 given 2

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,10 +3,15 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
+  FizzBuzz fizzBuzz = new FizzBuzz();
 
   @Test
   public void should_return_1_given_1() {
-    FizzBuzz fizzBuzz = new FizzBuzz();
     assertEquals(1, fizzBuzz.call(1));
+  }
+
+  @Test
+  public void should_return_2_given_2() {
+    assertEquals(2, fizzBuzz.call(2));
   }
 }
